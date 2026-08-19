827276daa0caee1363bb79153a627df2267537da
Linus Sjölinder <linus.sjolinder@b3.se>
2026-08-19 14:36:05 +0200

    HYDRAN-2439: use the Month constant in the node entity test
    SonarQube java:S8694 on the pull request.

--- src/test/java/se/sundsvall/memories/integration/db/model/NodeEntityTest.java
+++ src/test/java/se/sundsvall/memories/integration/db/model/NodeEntityTest.java
@@ -11,6 +11,7 @@ import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanEqualsExclud
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanHashCodeExcluding;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToStringExcluding;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static java.time.Month.JANUARY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -35,7 +36,7 @@ class NodeEntityTest {
 	@Test
 	void testBuilderMethods() {
 		final var nodeType = NodeTypeEntity.create().withId(1).withName("Arkiv");
-		final var deletedDate = LocalDate.of(2026, 1, 15);
+		final var deletedDate = LocalDate.of(2026, JANUARY, 15);
 
 		final var result = NodeEntity.create()
 			.withId(100)
